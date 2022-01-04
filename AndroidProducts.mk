popoA3M-World/device_10or_E-a11

E: Rebrand to Tenor E
Signed-off-by: popoASM <pratyayaborborah@gmail.com>

AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_land.mk
+    $(LOCAL_DIR)/lineage_E.mk

nekodev17/device

AndroidProducts.mk
@@ -5,4 +5,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_land.mk
+
+    $(LOCAL_DIR)/aosp_E.mk