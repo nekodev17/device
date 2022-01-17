50e1cf466f16d87f331622f45bc4f7ab85dbd6e2
nekodev17 <nekodev17@nekodev17>
2022-01-17 18:41:19 +0530

    Add lineage support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,5 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# LineageOS (crDroid)
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_E.mk
+    $(LOCAL_DIR)/lineage_E.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_E-userdebug \
+    lineage_E-user \
+    lineage_E-eng
